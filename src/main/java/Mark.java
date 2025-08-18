e7d428b4efee4f6beacd80834d9533b1483ead3c
junhao4 <e1406393@u.nus.edu>
2025-08-18 17:15:22 +0800

    Add skeletal version

---
+++ src/main/java/Mark.java
@@ -0,0 +1,15 @@
+public class Mark {
+    public static void main(String[] args) {
+
+        String line = "____________________________________________________________\n";
+
+        System.out.println(
+            line +
+            "Hello! I'm Mark\n" +
+            "What can I do for you?\n" +
+            line +
+            "Bye. Hope to see you again soon!\n" +
+            line
+        );
+    }
+}
