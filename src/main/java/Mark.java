45e3007e160245ee2e93eecdb613e39b11ae8024
junhao4 <e1406393@u.nus.edu>
2025-08-18 17:37:35 +0800

    Make Mark echo commands
    Add functionality to take user input and echos that
    Exits when the user types the command bye

--- src/main/java/Mark.java
+++ src/main/java/Mark.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Mark {
     public static void main(String[] args) {
 
-        String line = "____________________________________________________________\n";
+        String line = "    ____________________________________________________________\n";
 
         System.out.println(
             line +
-            "Hello! I'm Mark\n" +
-            "What can I do for you?\n" +
+            "    Hello! I'm Mark\n" +
+            "    What can I do for you?\n" +
+            line
+        );
+
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            String msg = sc.nextLine();
+            if (msg.equals("bye")) {
+                break;
+            }
+            System.out.println(
+                line + "    " + msg + "\n" + line
+            );
+        }
+
+        System.out.println(
             line +
-            "Bye. Hope to see you again soon!\n" +
+            "    Bye. Hope to see you again soon!\n" +
             line
         );
     }
