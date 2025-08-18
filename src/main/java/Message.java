2ed919e892c1e4b144f83dc8c44aa3d082b7e462
junhao4 <e1406393@u.nus.edu>
2025-08-18 19:21:04 +0800

    Add more methods to Message

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -8,21 +8,27 @@ public class Message {
     public static void intro() {
         System.out.println(
             Message.line(
-            "    Hello! I'm Mark\n" +
-                 "    What can I do for you?\n"
+            "     Hello! I'm Mark\n" +
+                 "     What can I do for you?\n"
             )
         );
     }
 
     public static void bye() {
         System.out.println(
-                Message.line("    Bye. Hope to see you again soon!\n")
+                Message.line("     Bye. Hope to see you again soon!\n")
+        );
+    }
+
+    public static void println(String msg) {
+        System.out.println(
+                Message.line("     " + msg + "\n")
         );
     }
 
     public static void print(String msg) {
         System.out.println(
-                Message.line("    " + msg + "\n")
+                Message.line(msg)
         );
     }
 }
