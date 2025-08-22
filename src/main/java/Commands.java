b836ce751170e2b0824863370aaab4f1620245b0
junhao4 <e1406393@u.nus.edu>
2025-08-22 22:44:19 +0800

    refactor code to use enums

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,12 +1,20 @@
 public enum Commands {
-    MARK(),
-    UNMARK(),
-    TODO(),
-    DEADLINE(),
-    EVENT(),
-    LIST(),
-    DELETE(),
-    BYE();
+    MARK(MarkCommand::new),
+    UNMARK(UnmarkCommand::new),
+    TODO(TodoCommand::new),
+    DEADLINE(DeadlineCommand::new),
+    EVENT(EventCommand::new),
+    LIST(ListCommand::new),
+    DELETE(DeleteCommand::new),
+    BYE(ByeCommand::new);
 
+    private final CommandBuilder cmdBuilder;
 
+    Commands(CommandBuilder cmdBuilder) {
+        this.cmdBuilder = cmdBuilder;
+    }
+
+    public void createAndExecute(String arg, TaskList taskList) throws MarkExceptions {
+        this.cmdBuilder.create(arg, taskList).execute();
+    }
 }
